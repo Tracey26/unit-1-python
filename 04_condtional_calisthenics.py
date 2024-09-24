f98bd207d2bcaed890e14b0851039093f87b27f9
Tracey26 <tordonez@brooklynsteamcenter.org>
2024-09-23 21:45:20 -0400

    finished conditionals

--- 04_condtional_calisthenics.py
+++ 04_condtional_calisthenics.py
@@ -8,7 +8,11 @@ Return True if both conditions are met, False otherwise.
 num = int(input("Enter a digit even to or greater than 10: "))
 #checks if value of number is equal to or greater than 10
 if num >= 10:
+    #is printed if number is equal to 10 or greater
     print("true")
+else:
+    #is printed if number is NOT equal to 10 or greater
+    print("false")
 
 '''
 Exercise 2:
@@ -37,11 +41,50 @@ If it's not, print "No, that fruit is not in the list."
 fruit = ["strawberry","banana","apple","pear"]
 #asks user to input a fruit name
 name= input("Name a fruit:")
+#checks if users input is already in the list
+if name in fruit:
+    print("Yes, that fruit is in the list.")
+#will print if the named item is not in the list
+else:
+    print ("No, that fruit is not in the list.")
 
 '''
 Exercise 4:
 Check if a year is a century year and a leap year.
 '''
+#prompts user to input a year
+year = int(input("Enter a year: "))
+
+#checks if the year is divisble by 100
+def century(year):
+    return year % 100 == 0
+
+#checks if year is divisble by 4
+def leap(year):
+    if year % 4 == 0:
+        #checks if the year is divisble by 100
+        if year % 100 == 0:
+            #checks if it is divisble by 100 and if it is it will be divided by 400
+            return year % 400 == 0  
+        #will print that the year is a leap year if it is divisible by 4 and not 100
+        return True  
+        #will print if the year is not divisible by 4  
+    return False  
+
+
+def check(year):
+    #combines the checks done
+    if century(year) and leap(year):
+        #prints if the year is a century year and a leap year
+        print(f"{year} is a century year and a leap year.")
+    else:
+        #prints if the year is NOT a century year and a leap year
+        print(f"{year} is NOT a century year and a leap year.")
+#checks conditions in order to print the right message
+check(year)
+
+
+
 
 '''
 Exercise 5:
@@ -49,9 +92,43 @@ Calculate the cost of shipping for an online order based on the order weight and
 The shipping cost is $5 per kilogram for Zone A and $7 per kilogram for Zone B. 
 If the order weight is less than 0 kg, return an error message.
 '''
+#asks the user to input the weight and zone of their shipment 
+weight = float(input("Input weight in kilograms:"))
+zone = input("Enter zone A or B:").upper()
+
+
+     #calculates the cost based on the entered weight and zone (zone A) 
+if zone == 'A':
+        cost = weight * 5
+     #calculates the cost based on the entered weight and zone (zone B) 
+elif zone == 'B':
+        cost = weight * 7
+    #prints if the order weight is less than 0 kg
+else:
+        print("Error: Invalid weight")
+
+#prints cost
+print(cost)
+
 
 '''
 Exercise 6:
 Determine the type of a triangle based on side lengths.
 Equilateral, Isosceles, Scalene, or Not a triangle.
 '''
+#asks user to input values for the sides of the triangle
+a = float(input("Enter the length of side a:"))
+b = float(input("Enter the length of side b:"))
+c = float(input("Enter the length of side c:"))
+
+
+    #tests the triangle inequality theorem
+if a + b <= c or a + c <= b or b + c <= a:
+        print("Not a triangle: The sum of lengths of any two sides must be greater than length of third side. ")
+    #checks what type of triangle it is and prints the type
+if a == b == c:
+        print("Equilateral triangle")
+elif a == b or b == c or a == c:
+        print("Isosceles triangle")
+else:
+        print("Scalene triangle")
