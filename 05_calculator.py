fa2e7774788f8d2d07437b7f3369584594dd697f
Tracey26 <tordonez@brooklynsteamcenter.org>
2024-09-25 16:15:13 -0400

    Finished Calculatinator

--- 05_calculator.py
+++ 05_calculator.py
@@ -17,5 +17,56 @@ option = int(input(
     Confirm choice:
     """
 ))
-    if option = 1:
+#checks user's choice and if it is this one this code will be carried out
+if option == 1:
+    #carries out code and performs addition on user's input itegers
+    result1 = num1 + num2
+    #prints result of addition
+    print(f"The answer is {result1}")
+#checks user's choice and if it is this one this code will be carried out
+elif option == 2:
+    #carries out code and performs subtraction on user's input itegers
+    result2 = num1 - num2
+    #prints result of subtraction
+    print(f"The answer is {result2}")
+#checks user's choice and if it is this one this code will be carried out
+elif option == 3:
+    #carries out code and performs multiplication on user's input itegers
+    result3 = num1 * num2
+    #prints result of multiplication
+    print(f"The answer is {result3}")
+#checks user's choice and if it is this one this code will be carried out
+elif option == 4:
+    #checks if user input 0 as a denominator
+    if num2 != 0:
+        #carries out code and performs division on user's input itegers
+        result4 = num1 / num2
+        #prints result of division
+        print(f"The answer is {result4}")
+    #Will print if user input 0 as denominator
+    else:
+        print("Cannot calculate with denominator of 0!!!!!")
+#checks user's choice and if it is this one this code will be carried out
+elif option == 5:
+    #carries out code and performs floor division on user's input itegers
+    result5 = num1 ** num2
+    #prints result of floor division
+    print(f"The answer is {result5}")
+#checks user's choice and if it is this one this code will be carried out
+elif option == 6:
+    #carries out code and performs exponential on user's input itegers
+    result6 = num1 // num2
+    #prints result of exponential
+    print(f"The answer is {result6}")
+#checks user's choice and if it is this one this code will be carried out
+elif option == 7:
+    #carries out code and performs remainder on user's input itegers
+    result7 = num1 % num2
+    #prints result of remainder
+    print(f"The answer is {result7}")
+else:
+    #If the user doesn't pick a valid option this will be printed
+    print("Select a VALID operation!!")
+
+
 
